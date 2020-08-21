74ab332a2740f77a53290f1f2152a37a5c308de7
dmuruges <sekar.murugesan@gmail.com>
2020-08-21 14:59:28 +0530

    Implemented Memento Pattern

--- src/com/programmingwithdhana/Main.java
+++ src/com/programmingwithdhana/Main.java
@@ -1,8 +1,25 @@
 package com.programmingwithdhana;
+import com.programmingwithdhana.memento.Editor;
+import com.programmingwithdhana.memento.History;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Memento
+        var editor = new Editor();
+        var history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
+
     }
 }
